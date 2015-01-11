569283234a64c0600062b4f9a82ee051a0bf5358
Gary Bernhardt <gary.bernhardt@gmail.com>
2015-01-10 17:54:41 -0800

    updated app number logic

--- generatelogs.py
+++ generatelogs.py
@@ -7,7 +7,7 @@ logger = logging.getLogger("application_logger")
 logger.setLevel(logging.DEBUG)
 max_file_size = 1024 * 1024
 max_file_count = 0
-fh = logging.handlers.RotatingFileHandler("./application1.log", maxBytes=max_file_size,
+fh = logging.handlers.RotatingFileHandler("./application.log", maxBytes=max_file_size,
                                           backupCount=max_file_count)
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%Y-%m-%d %H:%M:%S')
@@ -16,10 +16,11 @@ logger.addHandler(fh)
 
 def log_data():
     import time
+    import random
+    app_number = random.randint(1,10)
     while 1:
-        import random
         random_log_level = random.choice([10, 20, 30, 40, 50])
-        logger.log(random_log_level, "This is a log message from application 1")
+        logger.log(random_log_level, "This is a log message from Application {0}".format(app_number))
         time.sleep(2)
 
 if __name__ == '__main__':
